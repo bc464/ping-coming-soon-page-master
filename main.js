5c35e29246575bf178de552de5239b7ad744ac94
brian <fil419@webmail.co.za>
2021-08-09 18:27:22 +0200

    received and coded files

---
+++ main.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('#btn');
+const inputEmail = document.querySelector("input");
+const hiddenPara = document.querySelector("#hiddenPara");
+
+btn.addEventListener('click', function ()  {
+	
+	if(inputEmail.textContent = "abc"){
+		inputEmail.style.border = "1px solid hsl(354, 100%, 88%)";
+		hiddenPara.style.display = "block";
+	}
+});
