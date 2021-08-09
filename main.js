9d450bbfb9b428f7b5b5995bdc3c414def817472
brian <fil419@webmail.co.za>
2021-08-09 20:21:21 +0200

    files updated

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const inputEmail = document.querySelector("input");
 const hiddenPara = document.querySelector("#hiddenPara");
 
 btn.addEventListener('click', function ()  {
-	
 	if(inputEmail.textContent = "abc"){
 		inputEmail.style.border = "1px solid hsl(354, 100%, 88%)";
 		hiddenPara.style.display = "block";
+		inputEmail.style.opacity = "1";
 	}
 });
