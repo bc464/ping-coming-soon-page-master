e1adb5b29be81fc023d66d9f64d5af9077433a42
brian <fil419@webmail.co.za>
2021-08-09 23:12:13 +0200

    all coding files updated

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const btn = document.querySelector('#btn');
-const inputEmail = document.querySelector("input");
+let inputEmail = document.querySelector("input");
 const hiddenPara = document.querySelector("#hiddenPara");
+const hiddenPara2 = document.querySelector("#hiddenPara2");
+let complete =  "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
 
 btn.addEventListener('click', function ()  {
-	if(inputEmail.textContent = "abc"){
+	if(inputEmail.value.trim()== ""){
 		inputEmail.style.border = "1px solid hsl(354, 100%, 88%)";
 		hiddenPara.style.display = "block";
+		inputEmail.style.opacity = "1";
+	}
+	else if (inputEmail.innerHTML = complete){
+		inputEmail.style.border = "1px solid hsl(354, 100%, 88%)";
+		hiddenPara2.style.display = "block";
 		inputEmail.style.opacity = "1";
 	}
 });
